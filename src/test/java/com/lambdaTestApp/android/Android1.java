f11989819ddb4240988bd4793e4076c5316c81cd
mayank2193 <mayankmaurya2193@gmail.com>
2022-01-20 20:03:46 +0530

    first commit

---
+++ src/test/java/com/lambdaTestApp/android/Android1.java
@@ -0,0 +1,56 @@
+package com.lambdaTestApp.android;
+
+import java.net.URL;
+import java.util.List;
+import java.net.MalformedURLException;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Android1 {
+
+        String userName = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+
+        @Test
+        public void basicTest() throws MalformedURLException,
+                        InterruptedException {
+                DesiredCapabilities caps = new DesiredCapabilities();
+
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("deviceName", "Galaxy Tab S4");
+                caps.setCapability("platformVersion", "10");
+                caps.setCapability("isRealMobile", true);
+                caps.setCapability("project", "App Automation Demo");
+                caps.setCapability("build", "Android");
+                caps.setCapability("name", "Single Test");
+                caps.setCapability("app", "<app_url>");
+
+                AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                                new URL("https://" + userName + ":" + accessKey + "@beta-hub.lambdatest.com/wd/hub"),
+                                caps);
+                WebDriverWait wait = new WebDriverWait(driver, 10);
+                AndroidElement searchElement = (AndroidElement) wait
+                                .until(ExpectedConditions
+                                                .elementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+                searchElement.click();
+                AndroidElement insertTextElement = (AndroidElement) wait
+                                .until(ExpectedConditions.elementToBeClickable(
+                                                MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+                insertTextElement.sendKeys("LambdaTest");
+                Thread.sleep(5000);
+
+                List<AndroidElement> allProductsName = driver.findElementsByClassName("android.widget.TextView");
+                assert (allProductsName.size() > 0);
+
+                // The driver.quit statement is required, otherwise the test continues to
+                // execute, leading to a timeout.
+                driver.quit();
+        }
+}
