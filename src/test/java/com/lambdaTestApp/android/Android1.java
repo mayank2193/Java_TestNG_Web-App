73978cba4679dbdf55a3ca2b87a0a541fe3f1c6d
lambdapro <93367572+lambdapro@users.noreply.github.com>
2022-04-01 00:28:31 +0530

    Update Android1.java

--- src/test/java/com/lambdaTestApp/android/Android1.java
+++ src/test/java/com/lambdaTestApp/android/Android1.java
@@ -29,7 +29,7 @@ public class Android1 extends AppUploadAndroid{
                 caps.setCapability("platformName", "Android");
                 caps.setCapability("build", "Android");
                 caps.setCapability("name", "Single Test");
-                caps.setCapability("app", android_appurl);
+                caps.setCapability("app", "android_appurl");
 
                 AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
                                 new URL("https://" + userName + ":" + accessKey + "@beta-hub.lambdatest.com/wd/hub"),
