583e85fd978c529a5c0fe79b5d6eedc11fe8f545
lambdapro <93367572+lambdapro@users.noreply.github.com>
2022-04-01 00:36:39 +0530

    Update AppUpload.java

--- src/test/java/com/lambdaTestApp/iOS/AppUpload.java
+++ src/test/java/com/lambdaTestApp/iOS/AppUpload.java
@@ -10,9 +10,9 @@ public class AppUpload {
     public static String userName = System.getenv("LT_USERNAME");
     public static String accessKey = System.getenv("LT_ACCESS_KEY");
 
-    public String upload() throws IOException {
-
+ public static void upload1() throws IOException {
         String credential = Credentials.basic(userName, accessKey);
+
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
@@ -20,7 +20,7 @@ public class AppUpload {
                 .addFormDataPart("appFile", "apps/lambdatest.ipa",
                         RequestBody.create(MediaType.parse("application/octet-stream"),
                                 new File("apps/lambdatest.ipa")))
-                .addFormDataPart("name", "Demo")
+                .addFormDataPart("custom_id","iOS_appurl")
                 .build();
         Request request = new Request.Builder()
                 .url("https://manual-api.lambdatest.com/app/upload/realDevice")
@@ -28,8 +28,5 @@ public class AppUpload {
                 .addHeader("Authorization", credential)
                 .build();
         Response response = client.newCall(request).execute();
-        String a = response.body().string().substring(77,108);
-        System.out.println(a);
-        return a;
     }
 }
