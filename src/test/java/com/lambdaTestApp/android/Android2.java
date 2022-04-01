95c022a01e28e6056dd4d82d4da5ea1073402369
mayank2193 <mayankmaurya2193@gmail.com>
2022-04-01 17:27:47 +0530

    in sync with main

---
+++ src/test/java/com/lambdaTestApp/android/Android2.java
@@ -0,0 +1,56 @@
+package com.lambdaTestApp.android;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.net.MalformedURLException;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Android2 extends AppUploadAndroid {
+
+        String userName = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
+        String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
+
+        @Test
+        public void basicTest() throws IOException,
+                InterruptedException {
+                upload();
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("deviceName", "Google Pixel 4");
+                caps.setCapability("platformVersion", "11");
+                caps.setCapability("isRealMobile", true);
+                caps.setCapability("build", "Android");
+                caps.setCapability("name", "Single Test");
+                caps.setCapability("app", "android_appurl");
+
+                AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                                new URL("https://" + userName + ":" + accessKey + "@beta-hub.lambdatest.com/wd/hub"),
+                                caps);
+                WebDriverWait wait = new WebDriverWait(driver, 10);
+                AndroidElement searchElement = (AndroidElement) wait
+                                .until(ExpectedConditions
+                                                .elementToBeClickable(MobileBy.AccessibilityId("Search Wikipedia")));
+                searchElement.click();
+                AndroidElement insertTextElement = (AndroidElement) wait
+                                .until(ExpectedConditions.elementToBeClickable(
+                                                MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+                insertTextElement.sendKeys("LambdaTest");
+                Thread.sleep(5000);
+
+                List<AndroidElement> allProductsName = driver.findElementsByClassName("android.widget.TextView");
+                assert (allProductsName.size() > 0);
+
+                // The driver.quit statement is required, otherwise the test continues to
+                // execute, leading to a timeout.
+                driver.quit();
+        }
+}
---
+++ src/test/java/com/lambdaTestApp/iOS/AppUpload.java
@@ -0,0 +1,32 @@
+package com.lambdaTestApp.iOS;
+
+import okhttp3.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AppUpload {
+
+    public static String userName = System.getenv("LT_USERNAME");
+    public static String accessKey = System.getenv("LT_ACCESS_KEY");
+
+ public static void upload1() throws IOException {
+        String credential = Credentials.basic(userName, accessKey);
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("appFile", "apps/lambdatest.ipa",
+                        RequestBody.create(MediaType.parse("application/octet-stream"),
+                                new File("apps/lambdatest.ipa")))
+                .addFormDataPart("custom_id","iOS_appurl")
+                .build();
+        Request request = new Request.Builder()
+                .url("https://manual-api.lambdatest.com/app/upload/realDevice")
+                .method("POST", body)
+                .addHeader("Authorization", credential)
+                .build();
+        Response response = client.newCall(request).execute();
+    }
+}
